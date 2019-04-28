25f67a339552cbf9e1bfe01cb5794ade464b2743
Bigbos starla <48884075+yordankeres123@users.noreply.github.com>
2019-04-28 11:11:37 +0700

    Add files via upload

---
+++ tools.sh
@@ -0,0 +1,114 @@
+clear
+figlet GREEN | lolcat
+toilet -f big -F gay DRAGON
+echo
+echo "8888888888   888888888888"
+echo "88      oo   88          "
+echo "88      oo   88          "
+echo "88   88888   888888888888"
+echo "88   88888             88"
+echo "88      88             88"
+echo "8888888888   888888888888"
+echo
+echo "<><><><><><><><><><><><><><><>"
+echo "MY TEAM CYB3R POP1CE +62 " | lolcat
+echo "FRIENDS MY TEAM HAPPY HACKING TEAM" | lolcat
+echo "I'M GREEN DRAGON / BIGBOS STARLA " | lolcat
+echo "Subscribe my chanel Bigbos Starla " | lolcat
+echo "<><><><><><><><><><><><><><><>" | lolcat
+echo
+sleep 2
+figlet GD | lolcat
+toilet -f big -F gay BS | lolcat
+sleep 5
+clear
+echo "MENU TOOLSNYA SILAHKAN DIPILIH"
+echo " 1. INSTALL TOOLSBRUTAL " | lolcat
+echo " 2. Ip Doos Untuk Doos Ip orang" | lolcat
+echo " 0. Kluar dari tools" | lolcat
+echo "Silahkan dipilih (1) jangan (01) bro"
+echo "kalau ada (0) bakal syntax error" | lolcat
+read -p"Bigbos Starla~■■" cok
+if [ $cok = 1 ]
+then
+clear
+toilet -f big -F gay GREEN
+figlet DRAGON | lolcat
+echo "■■■■■■■■■"
+echo "■■■■■■■■■"
+sleep 1
+echo "■■■■■■■■■■■■■■■"
+echo "■■■■■■■■■■■■■■■"
+sleep 1
+echo "■■■■■■■■■■■■■■■■■■■■■■■"
+echo "■■■■■■■■■■■■■■■■■■■■■■■"
+sleep 1
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+sleep 1
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+sleep 1
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+sleep 1
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+sleep 3
+echo "1."
+sleep 1
+echo "2."
+sleep 1
+echo "3."
+sleep 1
+echo "4."
+sleep 1
+echo "5."
+sleep 1
+echo "Downloading" | lolcat
+echo " Install Tools Berutal" | lolcat
+sleep 2
+apt update
+apt upgrade
+apt install git
+git clone https://github.com/Screetsec/Brutal
+mv  Brutal $HOME
+cd $HOME/Brutal
+sh Brutal.sh
+fi
+
+if [ $cok = 2 ]
+then
+clear
+figlet BIGBOS STARLA | lolcat
+apt update && apt upgrade
+apt install git
+apt install python
+apt install python2
+git clone https://github.com/CiKu370/ipddos.git
+mv ipddos $HOME
+cd $HOME/ipddos
+echo "[Ddos Pake Ip + port]"
+echo "[Contoh 127.x.x.x 8080]"
+read -p "[Masukan Ip dan port]=>:" target
+python2 ipddos.py target
+fi
+
+if [ $cok = 0 ]
+then
+clear
+toilet GREEN | lolcat
+figlet DRAGON | lolcat
+echo
+echo "I'M BIGBOS STARLA DISEBUT DENGAN GREEN DRAGON" | lolcat
+sleep 2
+echo "I'M TEAM CYB3R POP1CE +62"
+sleep 2
+echo "PLIS SUBSCRIBE I CHANEL Bigbos Starla" | lolcat
+sleep 2
+echo "THX TO -ARULL -MR.HANZ -MR.GHOST -MR.BAJINGAN -MR.DENGKLONG -ALL TEAM CYB3R "
+sleep 2
+echo "Makasih Sampai Jumpa Lagi Ditools Ini tunggu Tools v5 yahh" | lolcat
+exit
+fi
+
